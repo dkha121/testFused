674fd68e6724406ee3f86ee6a6eb85d1d065bebb
tien-ngnvan <tien.ngnvan@gmail.com>
2023-03-09 01:05:33 +0700

    chore:add docs

--- datareader/data_reader.py
+++ datareader/data_reader.py
@@ -1,8 +1,12 @@
 from abc import abstractmethod, ABC
+from typing import List, Dict, Union
 
 
 class DataReader(ABC):
     def __init__(self, data_path: str, sample_path: str):
+        """
+        Preprocessing dataset format
+        """
         self.data_path = data_path
         self.sample_path = sample_path
 
@@ -27,15 +31,31 @@ class DataReader(ABC):
         print(f"\nInput sample file is save to {self.sample_path}")
 
     @abstractmethod
-    def load_data(self):
+    def load_data(self) -> Union[List[Dict], Dict[str]]:
+        """
+        Read the data file (.json, .csv, ...)
+        :return: the list of dictionaries or the dictionary of samples in the dataset
+        """
         pass
 
     @abstractmethod
-    def get_utterance(self):
+    def get_utterance(self) -> List[List[str]]:
+        """
+        Implement your convert logics to get utterances (<=5 utterance)
+        :return: the list of list, each list contain utterances for each Input
+                EX: [[utterance1, utterance2, utterance3, ...], [utterance4, utterance5, utterance6, ...]]
+        """
         pass
 
     @abstractmethod
-    def define_input(self):
+    def define_input(self) -> List[Dict[str, str]]:
+        """
+        Define the training sample.
+        :return: list of dictionaries with two keys:
+            - 'prompt': the sample
+            - 'output': the label
+            EX: [{'output': ******, 'prompt': *******}, {'output': ******, 'prompt': *******}, ...]
+        """
         pass
 
 
