7ec7077377bda1db6d833ff3dd189c8af8f1b011
Liem_Truc <khakhaq121@gmailcom>
2023-03-09 18:57:18 +0700

    chore: add argument for fusedchat

--- datareader/data_converter.py
+++ datareader/data_converter.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("./")
 
+from datareader.fusedchat_reader import FUSEDCHATReader
 from datareader.ketod_reader import KETODReader
 import argparse
 
@@ -9,7 +10,8 @@ def make_parser():
     parser = argparse.ArgumentParser("Dataset Converter")
     parser.add_argument("--ketod_dataset", type=str, help="the path of ketod dataset")
     parser.add_argument("--ketod_sample", type=str, help="the path of ketod sample (out file)")
-
+    parser.add_argument("--fusedchat_dataset", type=str, help="the path of ketod dataset")
+    parser.add_argument("--fusedchat_sample", type=str, help="the path of ketod sample (out file)")
     return parser
 
 
@@ -18,10 +20,15 @@ def ketod_converter(data_path, sample_path):
     ketod.__call__()
 
 
+def fusedchat_converter(data_path, sample_path):
+    fusedchat = FUSEDCHATReader(data_path, sample_path)
+    fusedchat.__call__()
+
 if __name__ == '__main__':
     args = make_parser().parse_args()
     print("\nStarting to convert KETOD" + "."*10)
-    ketod_converter(args.ketod_dataset, args.ketod_sample)
+    fusedchat_converter(args.ketod_dataset, args.fusedchat_sample)
+    ketod_converter(args.fusedchat_dataset, args.ketod_sample)
     print("Complete converting KETOD")
 
 
