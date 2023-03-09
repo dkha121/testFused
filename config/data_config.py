bdf11b07c7ec9c87aced8e9657e16bce3d757f8f
Tuan-Pham <weekend.2810@gmail.com>
2023-03-09 13:39:27 +0700

    feat: New reader for WOI

--- config/data_config.py
+++ config/data_config.py
@@ -3,8 +3,9 @@ EOT_SEP = "[EOT]. "
 USER_SEP = "USER: "
 SYSTEM_SEP = "SYSTEM: "
 
-LIST_ACT = "user_action: inform, request, greeting, bye, general; "
-LIST_RULE = "1. Seek, 2. General, 3. Database: "
+LIST_ACT = "user_action: inform, request, inform-intent, negate-intent, affirm-intent, affirm, negate, select, " \
+           "request-alts, thank, greet, bye, general; "
+LIST_RULE = "1. Seek, 2. Chitchat, 3. Database: "
 DOMAIN_BUS = "Bus; Slot: day, departure, destination, leaveat. "
 
 INSTRUCTION1 = f"Instruction: In this task given a dialogue as context you must be given the type of belief state " \
@@ -25,11 +26,20 @@ INSTRUCTION5 = f'Instruction: Please predict the type of belief state between tw
 INSTRUCTION6 = f'Instruction: Let give the belief state between two persons or two speakers base on ' \
                f'this dialogue [CTX]<DIALOGUE_CONTEXT>[EOD]. ' \
                f'[OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN> [Q] What is the belief state? '
-INSTRUCTION7 = 'INSTRUCTION7 '
-INSTRUCTION8 = 'INSTRUCTION8 '
-INSTRUCTION9 = 'INSTRUCTION9 '
-INSTRUCTION10 = 'INSTRUCTION10 '
-INSTRUCTION11 = 'INSTRUCTION11'
+INSTRUCTION7 = f"Instruction: Given a dialogue history of two specified speakers as context, " \
+               f"identify the type of belief state between them. [CTX]<DIALOGUE_CONTEXT>[EOD]. " \
+               f'[OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN> [Q] What is the belief state between the specified speakers ?'
+INSTRUCTION8 = f"Instruction: Identify the type of belief state between two specified speakers " \
+               f"based on the following conversation context [CTX]<DIALOGUE_CONTEXT>[EOD]. " \
+               f'[OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN> [Q] What is the type of belief state that the specified speaker has ?'
+INSTRUCTION9 = f'Instruction: Given a dialogue history as context, identify the belief state over the course of the conversation.' \
+               f'[CTX]<DIALOGUE_CONTEXT>[EOD]. [OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN> [Q] What is the current belief state based on the above context ?'
+INSTRUCTION10 = f'Instruction: Determine the speaker goal and action based on the dialogue history between specified people or speakers as context.' \
+                f' [CTX]<DIALOGUE_CONTEXT>[EOD]. [OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN> ' \
+                f'[Q] What is the goal and action ?'
+INSTRUCTION11 = f'Instruction: Based on the following conversation context [CTX]<DIALOGUE_CONTEXT>[EOD],' \
+                f' generate a belief state of the speaker using the following options' \
+                f' [OPT] {LIST_ACT}; {LIST_RULE} <DOMAIN>. [Q] What is the belief state ? '
 INSTRUCTION12 = 'INSTRUCTION12'
 INSTRUCTION13 = 'INSTRUCTION13'
 INSTRUCTION14 = 'INSTRUCTION14'
--- datareader/data_converter.py
+++ datareader/data_converter.py
@@ -1,7 +1,9 @@
+
 import sys
 sys.path.append("./")
 
 from datareader.ketod_reader import KETODReader
+from datareader.WoI_reader import WOIReader
 import argparse
 
 
@@ -9,7 +11,8 @@ def make_parser():
     parser = argparse.ArgumentParser("Dataset Converter")
     parser.add_argument("--ketod_dataset", type=str, help="the path of ketod dataset")
     parser.add_argument("--ketod_sample", type=str, help="the path of ketod sample (out file)")
-
+    parser.add_argument("--woi_dataset", type=str, help="the path of Wizard of internet dataset")
+    parser.add_argument("--woi_sample", type=str, help="the path of  Wizard of internet sample (out file)")
     return parser
 
 
@@ -17,6 +20,9 @@ def ketod_converter(data_path, sample_path):
     ketod = KETODReader(data_path, sample_path)
     ketod.__call__()
 
+def woi_converter(data_path, sample_path):
+    woi = WOIReader(data_path, sample_path)
+    woi.__call__()
 
 if __name__ == '__main__':
     args = make_parser().parse_args()
@@ -24,4 +30,8 @@ if __name__ == '__main__':
     ketod_converter(args.ketod_dataset, args.ketod_sample)
     print("Complete converting KETOD")
 
+    print("\nStarting to convert WOI" + "." * 10)
+    woi_converter(args.woi_dataset,
+                  args.woi_sample)
+    print("Complete converting WOI")
 
