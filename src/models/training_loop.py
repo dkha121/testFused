5efe1f1f73cb9aa90a3c9e81b562252dc38aa234
Tuan-Pham <weekend.2810@gmail.com>
2023-04-04 11:01:23 +0700

    chore: save only on main process

--- src/models/training_loop.py
+++ src/models/training_loop.py
@@ -353,17 +353,14 @@ class Trainer:
                         logger.info(result["train_loss"])
                         logger.info(f"*** EVAL LOSS AT EPOCH {epoch} ***")
                         logger.info(result["eval_loss"])
-                        operations.send_to_device(result['eval_loss'], accelerator.device, non_blocking=True)
-                        operations.send_to_device(eval_losses, accelerator.device, non_blocking=True)
 
-                if self.output_dir is not None:
-                    accelerator.wait_for_everyone()
-                    if result['eval_loss'] == min(eval_losses):
-                        logger.info(f"***** Saving best eval loss epoch *****")
-                        logger.info(f"Saving epoch: {epoch}")
-                        self.save(accelerator, model, tokenizer, result)
-                    else:
-                        logger.info(f"***** Discarding epoch {epoch} *****")
+                    if self.output_dir is not None:
+                        if result['eval_loss'] == min(eval_losses):
+                            logger.info(f"***** Saving best eval loss epoch *****")
+                            logger.info(f"Saving epoch: {epoch}")
+                            self.save(accelerator, model, tokenizer, result)
+                        else:
+                            logger.info(f"***** Discarding epoch {epoch} *****")
 
             else:
                 result = {}
