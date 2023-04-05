69a1de4ace69e0751e4e8a533f665d4dba828f51
Tuan-Pham <weekend.2810@gmail.com>
2023-04-05 14:08:19 +0700

    chore: change config for bash file

--- run_training.sh
+++ run_training.sh
@@ -3,12 +3,12 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_defau
 	--output_dir "./output"  \
 	--train_files  "./ketod/train_sample.json" "./fused_chat/train_sample.json" "./woi/train_sample.json"\
 	--val_files   "./ketod/valid_sample.json" "./fused_chat/valid_sample.json" "./woi/valid_sample.json" \
-	--batch_size    2 \
+	--batch_size    14 \
 	--num_beams   4 \
 	--weight_decay  0.3 \
 	--learning_rate 5e-5 \
 	--mixed_precision fp16  \
-	--gradient_accumulation_steps 2 \
+	--gradient_accumulation_steps 4 \
 	--with_tracking  \
 	--report_to mlflow \
 	--checkpointing_steps epoch \
