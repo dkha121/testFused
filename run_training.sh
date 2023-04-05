b61b54958524e486e3387cf061f8dde61598030c
Tuan-Pham <weekend.2810@gmail.com>
2023-04-05 14:13:07 +0700

    chore: update config for bash file

--- run_training.sh
+++ run_training.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_defaultMultiGPU.yaml src/models/train.py 	\
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/train.py 	\
   --num_train_epochs 10 \
 	--output_dir "./output"  \
 	--train_files  "./ketod/train_sample.json" "./fused_chat/train_sample.json" "./woi/train_sample.json"\
 	--val_files   "./ketod/valid_sample.json" "./fused_chat/valid_sample.json" "./woi/valid_sample.json" \
-	--batch_size    14 \
+	--batch_size    16 \
 	--num_beams   4 \
 	--weight_decay  0.3 \
 	--learning_rate 5e-5 \
